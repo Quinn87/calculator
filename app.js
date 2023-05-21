ecc0c21de68faa7db115d8824b8e6f751485a1e6
Brandon Quinn <github@thequinn.org>
2023-05-21 10:52:23 -0500

    Added eventListener to capture keyed selection

--- app.js
+++ app.js
@@ -14,4 +14,16 @@ function divide (numOne, numTwo){
     return numOne / numTwo;
 }
 
-const numButtons = document.querySelectorAll('.numButton')
+let numOne = "";
+let numTwo = "";
+let operator = "";
+
+const numButtons = document.querySelectorAll('.numButton');
+const operatorButtons = document.querySelectorAll('.operatorButton');
+const screenText = document.querySelector('.screenText');
+
+numButtons.forEach(button => button.addEventListener('click', function (e) {
+    console.log(e.target.textContent);
+    screenText.innerText = e.target.textContent;
+    return numOne += e.target.textContent;
+}));
